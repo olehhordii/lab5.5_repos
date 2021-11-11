66a1b8f6d7f11d09a499dda20f0ce967ca972ada
olehhordii <oleh.hordii.ik.2021@lpnu.ua>
2021-11-11 15:34:26 +0200

    1 commit master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.5/lab5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t = 15;
+			double depth = 0;
+			double c;
+			c = C(6, 4, 1, depth);
+			Assert::AreEqual(c, t);
+		}
+	};
+}
---
+++ lab5.5/lab5.5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double C(double n, double k,double level , double &depth)
+{
+    if (level>depth)
+    {
+        depth=level ;
+    }
+    if (n > 0)
+    {
+        return C(n - 1, k - 1, level + 1, depth)+C(n-1,k, level + 1, depth);
+    }
+    if (k = n || k == 0) {
+        return 1;
+    }
+        
+
+    return 0;
+   
+}
+
+int main()
+{
+        double n, k;
+        double depth = 0;
+        cout << "enter n : ";
+        cin >> n;
+        cout << "enter k: ";
+        cin >> k;
+        cout <<"Binom = " << C(n, k, 1, depth) << endl;
+        cout << "depth = " << depth << endl;
+    
+    cin.get();
+    return 0;
+}
